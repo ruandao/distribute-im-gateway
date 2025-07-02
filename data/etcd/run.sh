f1a215dff384df9fb3384212e3a90eef8aa0d0d0
ruandao <ljy080829@gmail.com>
2025-07-02 15:41:37 +0800

    撰写 etcd 数据导入脚本 撰写 mysql 初始化脚本

--- data/etcd/run.sh
+++ data/etcd/run.sh
@@ -2,7 +2,7 @@
 set -x
 
 # 检查data.txt文件是否存在
-if [ -f "etcd/data.txt" ]; then
+if [ -f "data/etcd/data.txt" ]; then
     echo "开始导入初始数据..."
     
     # 从data.txt导入数据到etcd
@@ -13,9 +13,9 @@ if [ -f "etcd/data.txt" ]; then
         echo "$key $value"
         # 使用etcdctl导入数据
         docker exec etcd etcdctl put "$key" "$value"
-    done < "etcd/data.txt"
+    done < "data/etcd/data.txt"
     
     echo "数据导入完成！"
 else
-    echo "未找到etcd/data.txt文件，跳过数据导入。"
+    echo "未找到 data/etcd/data.txt 文件，跳过数据导入。"
 fi
