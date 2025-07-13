b8d3912a4a715264591aea8b291b459f5de50722
ruandao <ljy080829@gmail.com>
2025-07-13 16:42:00 +0800

    u

--- deploy/docker_swarm/run.sh
+++ deploy/docker_swarm/run.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 . bin/all.sh
 
-if need_build_img ruandao/im-auth:latest src/Auth pkg ;then
-    docker build --no-cache -t ruandao/im-auth:latest -f src/Auth/Dockerfile .
-fi
+# if need_build_img ruandao/im-auth:latest src/Auth pkg ;then
+#     docker build --no-cache -t ruandao/im-auth:latest -f src/Auth/Dockerfile .
+# fi
 
 
-# cd deploy/docker_swarm
+cd deploy/docker_swarm
+echo "ENV: $ENV" 
+yaml-merge .img.yml d.${ENV}.yml > docker-compose.yml
 
-# yaml-merge .img.yml d.prod.yml > docker-compose.yml
+
+# docker buildx imagetools inspect mysql:8.4
 
 
-# docker stack deploy -c docker-compose.yml im
+docker stack rm --detach=false im
+# 清理 mysql 数据
+rm -rf ~/distribute-im-gateway/deploy/data/mysql/volumes
+mkdir ~/distribute-im-gateway/deploy/data/mysql/volumes
+# sleep 3
+docker stack deploy --detach=false -c docker-compose.yml im
+
+# docker service logs im_mysql-test
--- deploy/money/ser-redis.tf
+++ deploy/money/ser-redis.tf
@@ -35,38 +35,7 @@ resource "aws_instance" "redis" {
         host        = self.public_ip  # 使用实例的公网IP
         timeout     = "5m"  # 设置超时时间
     }
-
-    # # 上传单个文件
-    # provisioner "file" {
-    #     source      = "scripts/swarm_join_worker.sh"  # 本地文件路径
-    #     destination = "/home/ec2-user/swarm_join_worker.sh"  # 远程目标路径
-    # }
     
-    # provisioner "remote-exec" {
-    #   inline = [ 
-    #     "cat /home/ec2-user/swarm_join_worker.sh",
-    #     "sed -i 's/__MANAGER_IP__/${aws_instance.swarm_manager.public_ip}/g' /home/ec2-user/swarm_join_worker.sh"
-    #    ]
-    # }
-    
-    # provisioner "local-exec" {
-    #     command = <<EOT
-    #     ssh -o StrictHostKeyChecking=no -i ~/.ssh/terraform-aws ec2-user@${aws_instance.swarm_manager.public_ip} " until test -f /home/ec2-user/swarm_token.json; do sleep 1; done; cat /home/ec2-user/swarm_token.json" | \
-    #     ssh -o StrictHostKeyChecking=no -i ~/.ssh/terraform-aws ec2-user@${self.public_ip} "cat > /home/ec2-user/swarm_token.json"
-    #     ssh -o StrictHostKeyChecking=no -i ~/.ssh/terraform-aws ec2-user@${self.public_ip} "sudo bash /home/ec2-user/swarm_join_worker.sh"
-    #     EOT
-    # }
-    # # 真实环境，请采用IAM之类的方式
-    # provisioner "file" {
-    #     source      = "../_ssh/terraform-aws"
-    #     destination = "/home/ec2-user/.ssh/terraform-aws"  # 远程目标路径
-    # }
-    # provisioner "remote-exec" {
-    #   inline = [ 
-    #     "chmod 0600 ~/.ssh/terraform-aws",
-    #     "ssh -o StrictHostKeyChecking=no -i ~/.ssh/terraform-aws ec2-user@${aws_instance.swarm_manager.public_ip} 'docker node update --label-add node-type=${self.tags.NodeType} ${self.public_ip}' ",
-    #    ]
-    # }
     provisioner "local-exec" {
       when = destroy
       command = templatefile("tpl/swarm_worker_destroy.tpl", {})
