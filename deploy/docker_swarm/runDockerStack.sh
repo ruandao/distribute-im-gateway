906f2fe2ba1a51c245a8bf13cc30e627e949d38e
ruandao <ljy080829@gmail.com>
2025-07-15 16:01:34 +0800

    separate command for `generate the docker compose file` and `run the docker stack`

--- deploy/docker_swarm/runDockerStack.sh
+++ deploy/docker_swarm/runDockerStack.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 set -x
-. bin/all.sh
-
-# if need_build_img ruandao/im-auth:latest src/Auth pkg ;then
-#     docker build --no-cache -t ruandao/im-auth:latest -f src/Auth/Dockerfile .
-# fi
-
-
-cd deploy/docker_swarm
-echo "ENV: $ENV" 
-yaml-merge .img.yml d.${ENV}.yml > docker-compose.yml
-
 
 # docker buildx imagetools inspect mysql:8.4
 
