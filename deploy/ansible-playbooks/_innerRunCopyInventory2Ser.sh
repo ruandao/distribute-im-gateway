465678b647f35162f6acc451cf93df9b491fa192
ruandao <ljy080829@gmail.com>
2025-07-18 16:06:42 +0800

    u

--- deploy/ansible-playbooks/_innerRunCopyInventory2Ser.sh
+++ deploy/ansible-playbooks/_innerRunCopyInventory2Ser.sh
@@ -7,5 +7,5 @@ pwd
 
 
 cd deploy
-scp -F _ssh/config -r _ssh mSer:~/distribute-im-gateway/deploy/_ssh 
-scp -F _ssh/config -r ansible-playbooks/inventory mSer:~/distribute-im-gateway/deploy/ansible-playbooks/inventory 
+scp -F _ssh/config -r _ssh mSer-0:~/distribute-im-gateway/deploy/_ssh 
+scp -F _ssh/config -r ansible-playbooks/inventory mSer-0:~/distribute-im-gateway/deploy/ansible-playbooks/inventory 
