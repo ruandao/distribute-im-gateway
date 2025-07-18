3de4d75ed833b9be4a3cb6e4d49643b2ab904d25
ruandao <ljy080829@gmail.com>
2025-07-18 22:55:09 +0800

    打通数据库隧道到本机3306端口

---
+++ deploy/_ssh/dbProxy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ps -ef | grep ssh | grep 3306 | awk '{print $2}'| xargs -I {} kill -9 {}
+ssh -o StrictHostKeyChecking=no -F deploy/_ssh/config -fN -L 3306:127.0.0.1:3306 dbSer-0
+ps -ef | grep 3306
