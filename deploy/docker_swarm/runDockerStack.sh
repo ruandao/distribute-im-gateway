3c573ff71a885978ab327d950451c9e435733ff6
ruandao <ljy080829@gmail.com>
2025-07-19 22:24:32 +0800

    将镜像拉取和 docker stack 部署分开

--- deploy/docker_swarm/runDockerStack.sh
+++ deploy/docker_swarm/runDockerStack.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x
 
-bash deploy/docker_swarm/pullAllImages.sh
 docker stack rm $dockerStackOpt im
 docker stack deploy $dockerStackOpt -c deploy/docker_swarm/docker-compose.yml im
