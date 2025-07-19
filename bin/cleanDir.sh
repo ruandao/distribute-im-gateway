3a60c436665ca56863c0337655d1978741978dc2
ruandao <ljy080829@gmail.com>
2025-07-20 00:39:17 +0800

    修改目录创建脚本

--- bin/cleanDir.sh
+++ bin/cleanDir.sh
@@ -8,12 +8,11 @@ function cleanDir() {
       rm -rf "$directory"
       echo "目录 $directory 已删除。"
       echo "目录 $directory 不存在。"
-      # 创建目录
-      # 创建目录
-      echo "创建目录 $directory ..."
-      mkdir -p "$directory"
-      echo `pwd`/$directory
-      ls -al `pwd`/$directory
-      echo "目录 $directory 创建成功。"
   fi
+  # 创建目录
+  echo "创建目录 $directory ..."
+  mkdir -p "$directory"
+  echo `pwd`/$directory
+  ls -al `pwd`/$directory
+  echo "目录 $directory 创建成功。"
 }
